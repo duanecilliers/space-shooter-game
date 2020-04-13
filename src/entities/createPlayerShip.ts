812d9f2f215c948e732d24bb0dd06c7d3c488497
Duane Cilliers <hello@duane.co.za>
2020-04-13 21:16:35 +0200

    Update player entity with shoot ability

--- src/entities/createPlayerShip.ts
+++ src/entities/createPlayerShip.ts
@@ -4,12 +4,18 @@ import hasPosition from 'components/hasPosition'
 import createState from 'utils/createState'
 import spriteConfig from 'configs/spriteConfig'
 import gameConfig from 'configs/gameConfig'
+import createLaserBeam from './createLaserGun'
 
 const createPlayerShip = function createPlayerShipFunc(scene: Phaser.Scene) {
   // variables and functions here are private unless listed below in localState.
   const state: any = {}
   let ship: Phaser.Physics.Arcade.Sprite
   let cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys
+  let spacebar
+  let gun
+  let animations: {
+    straightAnimation: string;
+  }
 
   function printInfo() {
     console.log(`name: %c${state.name}`, 'color: red')
@@ -21,17 +27,26 @@ const createPlayerShip = function createPlayerShipFunc(scene: Phaser.Scene) {
     ship = scene.physics.add.sprite(VIEWWIDTH / 2,VIEWHEIGHT - 50, spriteConfig.PLAYER_SHIP.KEY)
       .setScale(2)
 
-    const animations = createAnimations()
+    animations = createAnimations()
     ship.anims.play(animations.straightAnimation)
 
     ship.setCollideWorldBounds(true)
     cursorKeys = scene.input.keyboard.createCursorKeys()
+
+    spacebar = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+
+    /**
+     * @todo Rename beam gun
+     */
+    gun = createLaserBeam(scene)
+    gun.create()
   }
 
   /**
    * @todo add turning animations
    */
   function createAnimations () {
+    // move straight
     const straightKey = `${spriteConfig.PLAYER_SHIP.KEY}_anim`
     const straightframes = scene.anims.generateFrameNumbers(spriteConfig.PLAYER_SHIP.KEY, {})
     scene.anims.create({
@@ -61,7 +76,15 @@ const createPlayerShip = function createPlayerShipFunc(scene: Phaser.Scene) {
     }
   }
 
+  function shootBeam () {
+    if (Phaser.Input.Keyboard.JustDown(spacebar)) {
+      /** @todo add modifer for beam? */
+      gun.shoot(ship.x, ship.y, Math.random() > 0.5 ? 'ball' : 'beam')
+    }
+  }
+
   function update () {
+    shootBeam()
     playerMoveManager()
   }
 
